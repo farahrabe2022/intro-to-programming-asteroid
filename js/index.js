c6b10b1e2a37d28afa4093cfc8ff9944d489ff66
tianahR <43481097+tianahR@users.noreply.github.com>
2022-04-24 20:15:07 -0400

    Added more style with CSS to the page

--- js/index.js
+++ js/index.js
@@ -1,25 +1,33 @@
 // Add copyright to the footer thru DOM manipulation
 const today = new Date();
 const thisYear = today.getFullYear();
-const footer = document.querySelector('footer');
-const copyright = document.createElement('p');
+const copyright = document.querySelector('#copyright')
 copyright.innerHTML = " &copy Faratiana Rabenjamina "+ thisYear;
-footer.appendChild(copyright);
+
+
 
 // Create list of technical skills
 var skills = ["HTML", "CSS", "Javascript", "PHP", "MySQL", "BOOTSTRAP", "JELIX", "C++", "JAVA"];
-const skillsSection = document.getElementById('skills');
-var skillList = skillsSection.querySelector('ul');
 
-for(let i=0; i<skills.length; i++)
-{
-    //console.log(skills[i]);
-    // Inside the loop, create a new list item (li) element and store it in a variable named skill
-    let skill = document.createElement('li');
-    skill.innerText = skills[i];
-    skillList.appendChild(skill);
 
+// DOM selection
+const skillsSection = document.querySelector('#skills')
+const skillsList = skillsSection.querySelector('ul')
+
+// Inside the loop, create a new list item (li) element and store it in a variable named skill
+
+for (let i = 0; i < skills.length; i++) {
+  // DOM manipulation (create)
+  const skill = document.createElement('li')
+  skill.classList.add('tag')
+  skill.innerText = skills[i]
+
+  // DOM manipulation (modify)
+  skillsList.appendChild(skill)
 }
+
+
+
 
 // handle Message Form Submit
 
